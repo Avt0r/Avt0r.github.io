8684d565e734d26ef9fa271260ac8e6c36c0ac6b
Avt0r <105048851+Avt0r@users.noreply.github.com>
2024-11-18 00:21:37 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Пример функционала для формы обратной связи
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Форма отправлена!');
+});
